425efb2ee8ecd6ad06ae486fabe6f1eac785e511
Semo <ReneLoescher@gmx.net>
2021-08-11 19:13:00 +0200

    created components, that draw items in a grid and connect them with lines

---
+++ Assets/Scripts/LevelSelection/LevelSelectionGridBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SemoGames.LevelSelection
+{
+    public class LevelSelectionGridBehaviour : MonoBehaviour
+    {
+        [SerializeField] private List<Transform> _items;
+
+        [SerializeField] private Transform _startPoint;
+
+        [SerializeField] private int _spacing;
+        [SerializeField] private GameObject _templateLine;
+
+        private int _maxPerRow = 3;
+
+        private void Awake()
+        {
+            ArrangeItemsOnGrid();
+        }
+
+        private void ArrangeItemsOnGrid()
+        {
+            for (int i = 0; i < _items.Count; i++)
+            {
+                var currentItem = _items[i];
+
+                var startPosition = _startPoint.position;
+                Vector2 newPosition = new Vector2(
+                    (i / _maxPerRow) % 2 == 0
+                        ? startPosition.x + _spacing * (i % _maxPerRow)
+                        : startPosition.x + _spacing * ((_maxPerRow - 1) - i % _maxPerRow),
+                    startPosition.y - _spacing * (i / _maxPerRow));
+                currentItem.position = newPosition;
+
+                if (i > 0)
+                {
+                    DrawNewLine(_items[i-1].position, newPosition);
+                }
+            }
+        }
+
+        private void DrawNewLine(Vector3 point1, Vector3 point2)
+        {
+            var lineRenderer = Instantiate(_templateLine).GetComponent<LineRenderer>();
+            lineRenderer.SetPositions(new []{point1, point2});
+        }
+    }
+}
---
+++ Assets/Scripts/LevelSelection/LevelSelectionItemBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SemoGames.LevelSelection
+{
+    public class LevelSelectionItemBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+
+        private static readonly int IsOutlineActive = Shader.PropertyToID("_IsOutlineActive");
+
+        private void OnMouseEnter()
+        {
+            Debug.Log("Hi");
+            //_spriteRenderer.material.SetInt(IsOutlineActive, 1);
+        }
+
+        private void OnMouseExit()
+        {
+            Debug.Log("Bye");
+            //_spriteRenderer.material.SetInt(IsOutlineActive, 0);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Debug.Log("Hi");
+            _spriteRenderer.material.SetInt(IsOutlineActive, 1);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Debug.Log("Bye");
+            _spriteRenderer.material.SetInt(IsOutlineActive, 0);
+        }
+    }
+}
