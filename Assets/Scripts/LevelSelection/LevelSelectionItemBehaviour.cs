e1c4ed4a0a0b0bdb1f57ff2d557ad39537dfd3a5
Semo <ReneLoescher@gmx.net>
2021-09-26 20:45:04 +0200

    created tween to show coins under level selection items

--- Assets/Scripts/LevelSelection/LevelSelectionItemBehaviour.cs
+++ Assets/Scripts/LevelSelection/LevelSelectionItemBehaviour.cs
@@ -1,4 +1,7 @@
-﻿using Entitas;
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using Entitas;
 using Entitas.Unity;
 using TMPro;
 using UnityEngine;
@@ -6,10 +9,10 @@ using UnityEngine.EventSystems;
 
 namespace SemoGames.LevelSelection
 {
-    public class LevelSelectionItemBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    public class LevelSelectionItemBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler,
+        IPointerClickHandler
     {
-        [SerializeField]
-        private SpriteRenderer _spriteRenderer;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
 
         [SerializeField] private TMP_Text _levelIndexText;
 
@@ -17,7 +20,12 @@ namespace SemoGames.LevelSelection
 
         [SerializeField] private Color _notBeatenLevelColor;
 
+        [SerializeField] private GameObject _collectableTemplateObject;
+
         private int _levelIndex;
+
+        private List<GameObject> _spawnedCollectableObjects = new List<GameObject>(3);
+        private List<Tween> _activeTweens;
 
         public int LevelIndex
         {
@@ -28,7 +36,8 @@ namespace SemoGames.LevelSelection
                 _levelIndexText.text = _levelIndex.ToString();
 
                 IGroup<SaveDataEntity> savedLevelEntities =
-                    Contexts.sharedInstance.saveData.GetGroup(SaveDataMatcher.AllOf(SaveDataMatcher.Level, SaveDataMatcher.LevelIndex));
+                    Contexts.sharedInstance.saveData.GetGroup(SaveDataMatcher.AllOf(SaveDataMatcher.Level,
+                        SaveDataMatcher.LevelIndex));
 
                 foreach (SaveDataEntity saveDataEntity in savedLevelEntities.GetEntities())
                 {
@@ -38,8 +47,20 @@ namespace SemoGames.LevelSelection
                         return;
                     }
                 }
-                
+
                 _spriteRenderer.color = _notBeatenLevelColor;
+            }
+        }
+
+        private void Start()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject spawned = Instantiate(_collectableTemplateObject, Vector3.zero, Quaternion.Euler(0, 0, 0),
+                    transform);
+                spawned.SetActive(true);
+                spawned.transform.SetAsFirstSibling();
+                _spawnedCollectableObjects.Add(spawned);
             }
         }
 
@@ -49,6 +70,14 @@ namespace SemoGames.LevelSelection
             if (entityLink != null)
             {
                 ((GameEntity) entityLink.entity).isSelected = true;
+                CancelTweens();
+                for (var index = 0; index < _spawnedCollectableObjects.Count; index++)
+                {
+                    GameObject collectable = _spawnedCollectableObjects[index];
+                    Vector3 newPos = InstantiateInCircle(_spawnedCollectableObjects.Count, index);
+                    DOTween.To(() => collectable.transform.position, value => collectable.transform.position = value,
+                        newPos, 0.2f);
+                }
             }
         }
 
@@ -58,12 +87,41 @@ namespace SemoGames.LevelSelection
             if (entityLink != null)
             {
                 ((GameEntity) entityLink.entity).isSelected = false;
+                CancelTweens();
+                for (var index = 0; index < _spawnedCollectableObjects.Count; index++)
+                {
+                    GameObject collectable = _spawnedCollectableObjects[index];
+                    DOTween.To(() => collectable.transform.position, value => collectable.transform.position = value,
+                        transform.position, 0.2f);
+                }
             }
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
             Contexts.sharedInstance.game.CreateEntity().AddLevelSelected(_levelIndex);
+        }
+
+        private void CancelTweens()
+        {
+            if (_activeTweens != null && _activeTweens.Count > 0)
+            {
+                foreach (Tween activeTween in _activeTweens)
+                {
+                    activeTween.Kill();
+                }
+
+                _activeTweens.Clear();
+                _activeTweens = null;
+            }
+        }
+
+        private Vector3 InstantiateInCircle(int howMany, int index)
+        {
+            float radius = 1;
+            float angle = ((index + 1) * Mathf.PI) / (howMany + 1) + Mathf.PI / 2;
+            return transform.position +
+                   (new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0)) * radius;
         }
     }
 }
